eae07a60cec4bb5dd369fdd62bf86cc455a001c3
FThreef <FThreef.535@gmail.com>
2024-02-22 18:22:48 +0300

    6

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,19 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Authorization from './components/Authorization';
 import List from './components/List';
 
-const router = createBrowserRouter([
-	{
-		path: '/',
-		element: <Authorization />,
-	},
-	{
-		path: '/list',
-		element: <List />,
-	},
-]);
+const router = createBrowserRouter(
+	[
+		{
+			path: '/',
+			element: <Authorization />,
+		},
+		{
+			path: '/list',
+			element: <List />,
+		},
+	],
+	{ basename: import.meta.env.BASE_URL }
+);
 
 const App = () => {
 	return (
